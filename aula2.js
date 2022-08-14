7043ef7c3e9525b7396dffbcafbcb5561f88dd14
matheu <matheusviaira137@gmail.com>
2022-08-14 20:16:40 -0300

    temporario

---
+++ aula2.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+function t(){
+    let variavel = window.document.getElementByTagName("input");
+    window.document.write(variavel.innerHTML);
+}
+let p = window.document.getElementsByTagName("p")[0];
+window.document.write(p.innerText);
+
+
---
+++ templat.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>minha primeira aula de js</title>
+        <!--<link rel="stylesheet" href="index.css">-->
+        <style>
+            body {background-color: gray;}
+        </style>
+    </head>
+
+    <body>
+        <h1>hello, world.</h1>
+        <p>vou aprender a manipular <strong>DOM</strong> agora</p>
+        <div>uma dive isso aq!</div>
+        <input type="text" name="p1"/></br>
+        <input type="text" name="p2"/><br>
+        <button onclick="t()"/>button</button>
+        <script src="./aula2.js">
+        </script>
+    </body>
+</html>
+
