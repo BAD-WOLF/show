cece5f16965deb32d78290adb3cbd9f6029603f1
matheu <matheusviaira137@gmail.com>
2022-08-15 13:18:21 -0300

    temporario

--- templat.php
+++ templat.php
@@ -1,32 +1,17 @@
-<!DOCTYPE html>
-
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>minha primeira aula de js</title>
-        <!--<link rel="stylesheet" href="index.css">-->
-        <style>
-            p {position: center;}
-            body {background-color: gray;}
-        </style>
-    </head>
-
-    <body>
-        <center>
-            <h1>hello, world.</h1>
-            <p >vou aprender a manipular <strong>DOM</strong> agora</p>
-            <div>uma dive isso aq!</div>
-            <input id='input' type="text" name="p1"/></br>
-            <input type="text" name="p2"/><br>
-            <button onClick="t()">button</button></br>
-            <script>
-                function t(){
-                    let input = document.getElementsByTagName("input")[0];
-                    document.write(input.value);
-                }
-                let p = document.getElementsByTagName("p")[0];
-                document.write(p.value);
-            </script>
-        </center>
-    </body>
-</html>
+<body>
+    <center>
+    <h1>hello, world.</h1>
+    <p>vou aprender a manipular <strong>DOM</strong> agora</p>
+    <div>uma dive isso aq!</div>
+    <input id='input' type="text" name="p1" /></br>
+    <input type="text" name="p2" /><br>
+    <button type="submit" onClick="t()">button</button>
+    <script>
+        function t(e) {
+            e.preventDefault();
+            const variavel = document.getElementsByTagName('input')[0].value;
+            document.write(variavel);
+        }
+    </script>
+    </center>
+</body>
